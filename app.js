ce657ea6cf3fa1a6f4af21de49da228f9cd6ad43
alex88g <alexander.gallorini@gmail.com>
2025-04-18 13:30:56 +0200

    node-fetch test

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-const app = express();
-const port = 3000;
-
-app.use(cors());
-app.use(express.json());
 
 const authRoutes = require('./routes/auth');
 const bookingRoutes = require('./routes/bookings');
 const patientRoutes = require('./routes/patients');
-const doctorRoutes = require('./routes/doctors'); 
+const doctorRoutes = require('./routes/doctors');
 const chatRoutes = require('./routes/chat');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(cors());
+app.use(express.json());
 
 app.use('/api', authRoutes);
 app.use('/api/bookings', bookingRoutes);
 app.use('/api/patients', patientRoutes);
-app.use('/api/doctors', doctorRoutes); 
+app.use('/api/doctors', doctorRoutes);
 app.use('/api/chat', chatRoutes);
 
 app.get('/', (req, res) => {
