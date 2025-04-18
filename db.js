154a656e47f13b62254fdf1fb6b1891264916166
alex88g <alexander.gallorini@gmail.com>
2025-04-18 11:01:56 +0200

    Flyttade app.js till root och uppdaterade package.json

--- db.js
+++ db.js
@@ -1,8 +1,7 @@
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Anslut till Railway MySQL via fullständig URL
+
 const pool = mysql.createPool(process.env.DATABASE_URL);
 
-// Exportera connection poolen som Promise API
 module.exports = pool.promise();
