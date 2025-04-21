d44de40a46e8f4f3978b35cdca66a9bef5944d85
alex88g <alexander.gallorini@gmail.com>
2025-04-21 13:07:45 +0200

    test28

--- db.js
+++ db.js
@@ -1,7 +1,15 @@
-const mysql = require('mysql2');
+const mysql = require('mysql2/promise');
 require('dotenv').config();
 
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-const pool = mysql.createPool(process.env.DATABASE_URL);
-
-module.exports = pool.promise();
+module.exports = pool;
